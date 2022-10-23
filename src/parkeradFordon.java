177b095934660f6960461c1c5f3f051568d41065
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2022-10-23 23:53:53 +0200

    Update parkeradFordon.java

--- src/parkeradFordon.java
+++ src/parkeradFordon.java
@@ -1,12 +1,13 @@
-public class parkeradFordon { //klassen som håller brygga modeller och vikt
-
+public class parkeradFordon { //Skapar klassen parkeradeFordon
+    
+    //Definerar attributen för klassen
     String fordonsModell;
-
     int fordonsVikt;
     String lastBrygga;
-
-    public parkeradFordon(String lastBrygga, String fordonsModell, int fordonsVikt){
-        this.lastBrygga=lastBrygga;
+    
+    //Definerar konstruktorn med inparametrar String lastBrygga, String fordonsModell, int fordonsVikt.
+    public parkeradFordon(String lastBrygga, String fordonsModell, int fordonsVikt){ //Klassens Konstruktor,det måste vara samma namn som klassen dvs parkeradFordon
+        this.lastBrygga=lastBrygga; //this. står för det nuvarande objektets klassvariabel. 
         this.fordonsModell=fordonsModell;
         this.fordonsVikt=fordonsVikt;
     }
