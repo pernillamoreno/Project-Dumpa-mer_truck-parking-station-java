f1831a00098058f75fff5721b2c5fcc20a56242b
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2022-11-17 22:41:12 +0100

    Update parkeradFordon.java

--- src/parkeradFordon.java
+++ src/parkeradFordon.java
@@ -1,13 +1,13 @@
-public class parkeradFordon { //Klassen parkeradeFordon
-    
-    //Definerar attributen för klassen
+public class parkeradFordon {
+
     String fordonsModell;
+
     int fordonsVikt;
     String lastBrygga;
-    
-    //Definerar attributen för klassen, String lastBrygga, String fordonsModell, int fordonsVikt.
-    public parkeradFordon(String lastBrygga, String fordonsModell, int fordonsVikt){ 
-        this.lastBrygga=lastBrygga; //this. står för det nuvarande objektets klassvariabel. (Lite osäker här om jag gjort rätt i min beskrivning)
+
+
+    public parkeradFordon(String lastBrygga, String fordonsModell, int fordonsVikt){
+        this.lastBrygga=lastBrygga;
         this.fordonsModell=fordonsModell;
         this.fordonsVikt=fordonsVikt;
     }
@@ -15,4 +15,3 @@ public class parkeradFordon { //Klassen parkeradeFordon
         return lastBrygga+"\t\t" +"Fordon " + fordonsModell + "\t\t"+"Vikt:[" + fordonsVikt +" kg]";
     }
 }
-
