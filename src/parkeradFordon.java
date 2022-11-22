344972a4d90c1ad04fbb3d98b289fc9140f94f6c
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2022-11-22 12:02:05 +0100

    Update parkeradFordon.java

--- src/parkeradFordon.java
+++ src/parkeradFordon.java
@@ -7,9 +7,9 @@ public class parkeradFordon {
 
 
     public parkeradFordon(String lastBrygga, String fordonsModell, int fordonsVikt){
-        this.lastBrygga=lastBrygga;
         this.fordonsModell=fordonsModell;
         this.fordonsVikt=fordonsVikt;
+        this.lastBrygga=lastBrygga;
     }
     public String toString(){
         return lastBrygga+"\t\t" +"Fordon " + fordonsModell + "\t\t"+"Vikt:[" + fordonsVikt +" kg]";
